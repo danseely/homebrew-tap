3e4b278c03f752910b36afa05bea3b819fc3e7b0
Dan Seely <dseely@adadapted.com>
2026-04-15 21:06:43 -0400

    fix: add formula build dependencies

--- Formula/agendum.rb
+++ Formula/agendum.rb
@@ -8,7 +8,9 @@ class Agendum < Formula
   license "Apache-2.0"
 
   depends_on "gh"
+  depends_on "pkgconf" => :build
   depends_on "python@3.13"
+  depends_on "rust" => :build
 
   resource "annotated-doc" do
     url "https://files.pythonhosted.org/packages/57/ba/046ceea27344560984e26a590f90bc7f4a75b06701f653222458922b558c/annotated_doc-0.0.4.tar.gz"
